72d4f4535b34a39fa33886d76b7be02c5aeb1887
LAITH-RIHAN <135272940+LAITH-RIHAN@users.noreply.github.com>
2024-10-27 21:44:50 +0300

    hero section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,24 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import SearchForm from "../components/SearchForm";
 
 
-export default function Home() {
+export default async function Home({searchParams,}: {searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
+
   return (
-    <div className="text-2xl">
-      <h1>Home</h1>
+    <div>
+      <section className=" pink_container">
+      <h1 className="heading">
+          Pitch Your Startup, <br/>Connect With Entrepreneurs
+        </h1>
+
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
+          Competitions.
+        </p>
+        <SearchForm query={query}/>
+      </section>
     </div>
   );
 }
