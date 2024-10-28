6ff4748492c1ca4d0035eb33293377fcdea76716
LAITH-RIHAN <135272940+LAITH-RIHAN@users.noreply.github.com>
2024-10-28 15:13:23 +0300

    dynamic cards

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,27 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import SearchForm from "../components/SearchForm";
+import StartupCard, { StartupTypeCard } from "../components/StartupCard";
 
 
 export default async function Home({searchParams,}: {searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
+  const posts =[
+    {
+      _createdAt:new Date(),
+      views:55,
+      author:{
+              _id:1 ,
+              name:'Mike' ,
+              image :"https://plus.unsplash.com/premium_photo-1671656349322-41de944d259b?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            },
+      _id:1,
+      description:'this is the discriotios',
+      image:'https://images.unsplash.com/photo-1593376893114-1aed528d80cf?q=80&w=2030&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      category:'Robots',
+      title:'We Robots',
+    }
+  ] 
 
   return (
     <div>
@@ -18,6 +35,22 @@ export default async function Home({searchParams,}: {searchParams: Promise<{ que
           Competitions.
         </p>
         <SearchForm query={query}/>
+      </section>
+      
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupTypeCard , index:number) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
       </section>
     </div>
   );
--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,13 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+//this function for card publish date.
+
+export function formatDate(date: string) {
+  return new Date(date).toLocaleDateString("en-US", {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  });
+}
